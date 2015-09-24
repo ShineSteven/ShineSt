db97618d894aa27df1d696fc5c619917250c6bf9
Steven <fan.steven.chinag@gmail.com>
2015-09-25 02:25:09 +0800

    post "createAt" feature fix

--- app/common/DBUtils.scala
+++ app/common/DBUtils.scala
@@ -8,8 +8,6 @@ import play.api.Play.current
 object DBUtils {
   Class.forName("com.mysql.jdbc.Driver")
   val config = current.configuration
-//  def getConnection = DriverManager.getConnection("jdbc:mysql://localhost:3306/shine_st?user=shinest&password=shineGoal")
-  println(config.getString("DB.ip"))
   def connect = DriverManager.getConnection(s"jdbc:mysql://${config.getString("DB.ip").get}/${config.getString("DB.database").get}?user=${config.getString("DB.account").get}&password=${config.getString("DB.pw").get}")
 
   def close(closeable: AutoCloseable) {
