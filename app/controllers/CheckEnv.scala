34889091dade4cf39f7e00bbd611e87696a96e9f
Steven <fan.steven.chinag@gmail.com>
2015-11-18 03:07:57 +0800

    template upgrade, use semantic ui distinguish prod, dev conf

--- app/controllers/CheckEnv.scala
+++ app/controllers/CheckEnv.scala
@@ -11,7 +11,7 @@ import model.{ProviderContext, CustomizeContext}
 import play.api.mvc.{Action, Controller}
 
 @Singleton
-class CheckEnv extends Controller {
+class CheckEnv extends Controller  with ProviderContext {
   val config = ConfigFactory.load()
 
   var currentCount = 0
@@ -20,4 +20,10 @@ class CheckEnv extends Controller {
     currentCount += 1
     Ok(s"current env ${config.getString("env.debug")} : $currentCount")
   }
+
+
+  def show(id: Long) = Action { implicit request =>
+    Ok(views.html.sem_test_2(PostDao.queryPostById(id).head))
+  }
+
 }
